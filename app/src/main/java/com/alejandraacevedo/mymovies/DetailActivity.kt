df3c8eac463492e56e8d1af1ab53609768b5cb09
Alejandra ACEVEDO URREGO <W110747@fr01.awl.atosorigin.net>
2021-03-30 00:20:16 +0200

    feat: Create first Activity demonstration with viewbinding

---
+++ app/src/main/java/com/alejandraacevedo/mymovies/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.alejandraacevedo.mymovies
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/alejandraacevedo/mymovies/MainActivity.kt
+++ app/src/main/java/com/alejandraacevedo/mymovies/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.alejandraacevedo.mymovies
 
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
+import com.alejandraacevedo.mymovies.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitContent.setOnClickListener{
+            Toast.makeText(this, binding.inputText.text, LENGTH_SHORT).show()
+        }
+        Timber.d("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("onDestroy")
     }
 }
