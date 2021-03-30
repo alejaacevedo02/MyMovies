86b5133fa480bb587121471ce8d363a91cf8b822
Alejandra ACEVEDO URREGO <W110747@fr01.awl.atosorigin.net>
2021-03-30 20:41:39 +0200

    feat: Use RecyclerView and load images with Glide

--- app/src/main/java/com/alejandraacevedo/mymovies/MainActivity.kt
+++ app/src/main/java/com/alejandraacevedo/mymovies/MainActivity.kt
@@ -13,10 +13,15 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.submitContent.setOnClickListener{
-            Toast.makeText(this, binding.inputText.text, LENGTH_SHORT).show()
-        }
-        Timber.d("onCreate")
+        binding.moviesRv.adapter = MoviesAdapter(listOf(
+                Movie("Title 1", "https://loremflickr.com/320/240?lock=1"),
+                Movie("Title 2", "https://loremflickr.com/320/240?lock=2"),
+                Movie("Title 3", "https://loremflickr.com/320/240?lock=3"),
+                Movie("Title 4", "https://loremflickr.com/320/240?lock=4"),
+                Movie("Title 5", "https://loremflickr.com/320/240?lock=5"),
+                Movie("Title 6", "https://loremflickr.com/320/240?lock=6")
+        ))
+
     }
 
     override fun onDestroy() {
